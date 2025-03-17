030ae2a9e7ef2995dd6190957424b77bbab13669
jeancosta66 <costajean1005@gmail.com>
2025-03-17 14:25:57 -0300

    Update and rename README.md to desafio_01.py

--- README.md
+++
@@ -1,1 +0,0 @@
-# desafio_guanabara_1
---
+++ desafio_01.py
@@ -0,0 +1,6 @@
+# desafio_guanabara_1
+
+n1 = int(input("digite um valor: "))
+n2 = int(input("digite um outro valor: "))
+s = n1 + n2
+print("A soma entre {} e {} vale {}".format(n1, n2, s))
